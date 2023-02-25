e8504f404dd7704d794f647336018987118b1bb8
Sergey Antipov <ser_ok@mail.ru>
2023-02-26 01:29:40 +0300

    added Task 4 of Seminar 1

---
+++ Seminar_1/HomeTask_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Принимает на вход три числа и выдаёт максимальное из них
+
+Console.WriteLine("Введите три числа:");
+Console.Write("а = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.Write("c = ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+double max = a;
+if (max < b)
+    max = b;
+if (max < c)
+    max = c;
+Console.WriteLine("наибольшее из трёх чисел: " + max);
