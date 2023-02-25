8b1a7c926f8101162b42a990082f41b836f63c15
Sergey Antipov <ser_ok@mail.ru>
2023-02-26 01:43:36 +0300

    added Task 6 of Seminar 1

---
+++ Seminar_1/HomeTask_6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Определяет, является ли введенное число четным
+
+Console.Write("Введите число ");
+double a = Convert.ToDouble(Console.ReadLine());
+if ((a % 2) == 0)
+    Console.WriteLine(a + " - число четное");
+else
+    Console.WriteLine(a + " - число нечетное");
