8d6b4e81735a5b14158ca1ecfe6b27fb0881f55d
Sergey Antipov <ser_ok@mail.ru>
2023-02-26 02:15:42 +0300

    added Task 8 of Seminar 1

---
+++ Seminar_1/HomeTask_8/Program.cs
@@ -0,0 +1,18 @@
+﻿// Принимает на вход число N, а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите целое число N большее единицы ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (n > 1)
+{
+    while (count <= n)
+    {
+        if ((count % 2) == 0)
+            Console.Write(count + ", ");
+        count++;
+    }
+}
+else
+    Console.WriteLine("введено неверное число");
+Console.WriteLine();
