4ba78018e72c7fd6d9bd1a9b6563468286bc3084
Sergey Antipov <ser_ok@mail.ru>
2023-02-26 01:11:38 +0300

    added Task 2 of Seminar 1

--- Seminar_1/HomeTask_2/Program.cs
+++ Seminar_1/HomeTask_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход два числа и выдаёт, какое большее, а какое меньшее
+
+Console.Write("Введите два числа: а = ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("и b = ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+if (a < b)
+    Console.WriteLine("число " + b + " больше числа " + a);
+else
+{
+    if (a > b)
+        Console.WriteLine("число " + a + " больше числа " + b);
+    else
+        Console.WriteLine("числа равны");
+}
