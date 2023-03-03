d0f373868e21dc9e641d290509a1b56f378d3759
Sergey Antipov <ser_ok@mail.ru>
2023-03-03 22:45:17 +0300

    new solution of Task 10

--- Seminar_2/HomeTask_10/Program.cs
+++ Seminar_2/HomeTask_10/Program.cs
@@ -5,7 +5,23 @@
 // 782 -> 8
 // 918 -> 1
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine("Входное число " + num);
-num = num / 10 % 10;
-Console.WriteLine("Вторая цифра числа равна " + num);
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine("Входное число " + num);
+// num = num / 10 % 10;
+// Console.WriteLine("Вторая цифра числа равна " + num);
+
+///////////////////////////////////////////////////////
+
+int Rnd(int minRand, int maxRand)   // Генерация случайного целого числа
+{
+    return new Random().Next(minRand, maxRand);
+}
+
+int FindNumber(int inputNumber)     // Вычисление второй цифры трёхзначного числа
+{
+    return inputNumber / 10 % 10;
+}
+
+int number = Rnd(100, 1000);
+Console.Write("Для трёхзначного числа " + number);
+Console.WriteLine(" вторая цифра равна " + FindNumber(number));
