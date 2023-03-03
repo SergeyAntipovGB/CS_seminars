4543dc208d6f62236f5a21578f5f7ce0e3495bc9
Sergey Antipov <ser_ok@mail.ru>
2023-03-04 01:14:18 +0300

    new solution of Task 2 HARD

--- Seminar_2/HomeTask_2HARD/Program.cs
+++ Seminar_2/HomeTask_2HARD/Program.cs
@@ -13,21 +13,46 @@
 // В комнате может быть очень много программистов. Проверьте, что ваша
 // программа правильно обработает все случаи, как минимум до 1000 человек.
 
-Console.Write("Введите количество человек в комнате : ");
-int programmers = Convert.ToInt32(Console.ReadLine());
-int div = 1; //целочисленный остаток
-string index; //окончание слова
+// Console.Write("Введите количество человек в комнате : ");
+// int programmers = Convert.ToInt32(Console.ReadLine());
+// int div = 1; //целочисленный остаток
+// string index; //окончание слова
 
-if (programmers < 0) Console.WriteLine("Неверное число!");
-else 
+// if (programmers < 0) Console.WriteLine("Неверное число!");
+// else 
+// {
+//     if ((programmers < 15) && (programmers > 10)) index = "ов";
+//     else
+//     {
+//         div = programmers % 10;
+//         if (div == 1) index = "";
+//         else if ((div > 1) && (div < 5)) index = "а";
+//             else index = "ов";
+//     }
+//     Console.WriteLine($"В помещении находится {programmers} программист{index}");
+// }
+
+////////////////////////////////////////////////////////////
+
+int Rnd(int minRand, int maxRand)   // Генерация случайного целого числа
 {
-    if ((programmers < 15) && (programmers > 10)) index = "ов";
+    return new Random().Next(minRand, maxRand);
+}
+
+string Progs(int coint)     // Подбор окончания слова
+{
+    string index = "NO"; //окончание слова
+    int div = 1; //целочисленный остаток
+    if ((coint < 15) && (coint > 10)) index = "ов";
     else
     {
-        div = programmers % 10;
+        div = coint % 10;
         if (div == 1) index = "";
         else if ((div > 1) && (div < 5)) index = "а";
             else index = "ов";
     }
-    Console.WriteLine($"В помещении находится {programmers} программист{index}");
+    return index;
 }
+
+int programmers = Rnd(0, 1001);
+Console.WriteLine($"В помещении находится {programmers} программист{Progs(programmers)}");
