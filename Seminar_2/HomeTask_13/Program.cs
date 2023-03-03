7dde3f4230883cbb8465a974d8b2d7d80897401f
Sergey Antipov <ser_ok@mail.ru>
2023-03-04 00:09:38 +0300

    new solution of Task 13

--- Seminar_2/HomeTask_13/Program.cs
+++ Seminar_2/HomeTask_13/Program.cs
@@ -5,14 +5,38 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.Write("введите целое многозначное число: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if (num < 0) num = -num;
-if (num > 99)
+// Console.Write("введите целое многозначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num < 0) num = -num;
+// if (num > 99)
+// {
+//     while (num > 999) num = num / 10;
+//     num = num % 10;
+//     Console.WriteLine("третья цифра - " + num);
+// }
+// else
+//     Console.WriteLine("третьей цифры нет");
+
+//////////////////////////////////////////////////////////
+
+int Rnd(int minRand, int maxRand)   // Генерация случайного целого числа
 {
-    while (num > 999) num = num / 10;
-    num = num % 10;
-    Console.WriteLine("третья цифра - " + num);
+    return new Random().Next(minRand, maxRand);
 }
-else
-    Console.WriteLine("третьей цифры нет");
+
+void FindTherdNumber(int inputNumber)     // Обработка входящего числа, поиск третьей цифры
+{
+    if (inputNumber < 0) inputNumber = -inputNumber;
+    if (inputNumber > 99)
+        {
+        while (inputNumber > 999) inputNumber = inputNumber / 10;
+        inputNumber = inputNumber % 10;
+        Console.WriteLine("третья цифра - " + inputNumber);
+        }
+    else
+        Console.WriteLine("третьей цифры нет");
+}
+
+int randomNumber = Rnd(1, 100000000);
+Console.WriteLine("Случайное число " + randomNumber);
+FindTherdNumber(randomNumber);
