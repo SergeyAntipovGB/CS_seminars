33c55709c09250a2c06a8fd06d8ef192227a9ea1
Sergey Antipov <ser_ok@mail.ru>
2023-03-04 00:56:26 +0300

    new solution of Task 15

--- Seminar_2/HomeTask_15/Program.cs
+++ Seminar_2/HomeTask_15/Program.cs
@@ -5,11 +5,35 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Введите порядковый номер дня недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
-if ((day<1)||(day >7)) Console.WriteLine("такого номера нет в неделе!");
-else
+// Console.Write("Введите порядковый номер дня недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if ((day < 1) || (day > 7)) Console.WriteLine("такого номера нет в неделе!");
+// else
+// {
+//     if ((day == 6) || (day == 7)) Console.WriteLine("это выходной день!");
+//     else Console.WriteLine("Это не выходной день!");
+// }
+
+////////////////////////////////////////////////
+
+int DayOfWeek()     // Ввод порядкового номера дня недели с проверкой правильности
+{
+    int day=-1;
+    bool index = false;
+    while  (index == false)
     {
-    if ((day==6)||(day ==7)) Console.WriteLine("это выходной день!");
-    else Console.WriteLine("Это не выходной день!");
+        Console.Write("Введите порядковый номер дня недели: ");
+        day = Convert.ToInt32(Console.ReadLine());
+        if ((day < 1) || (day > 7)) Console.WriteLine("такого номера дня нет в неделе!");
+        else index = true;
     }
+    return day;   
+}
+
+void WhatADay(int numberDay)        // Вывод на консоль соответствующего дня
+{
+    if ((numberDay == 6) || (numberDay == 7)) Console.WriteLine("это выходной день!");
+    else Console.WriteLine("Это не выходной день!");
+}
+
+WhatADay(DayOfWeek());
