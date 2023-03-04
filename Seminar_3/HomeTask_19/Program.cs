a0456cd1a04d77aa3a1db592f7b3270009f80319
Sergey Antipov <ser_ok@mail.ru>
2023-03-04 22:25:02 +0300

    added Home Task 19 of Seminar 3

---
+++ Seminar_3/HomeTask_19/Program.cs
@@ -0,0 +1,34 @@
+﻿//================== Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом. Через строку решать нельзя.
+//
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int InputFiveDigitNumber()
+{
+    int inputNumber = -1;
+    bool crash = true;
+    while (crash == true)
+    {
+        Console.Write("Введите пятизначное число > ");
+        inputNumber = Convert.ToInt32(Console.ReadLine());
+        if ((inputNumber > 9999) && (inputNumber < 100000))
+            crash = false;
+        else
+            Console.WriteLine("Неправильное число !");
+    }
+    return inputNumber;
+}    
+
+void VerificateNumber(int palindrom)
+{
+    if ((palindrom / 10000 == palindrom % 10) && (palindrom / 1000 % 10 == palindrom % 100 / 10))
+        Console.WriteLine($"{palindrom} -> да");
+    else
+        Console.WriteLine($"{palindrom} -> нет");
+}
+
+
+VerificateNumber(InputFiveDigitNumber());
