ca598ad5f121bb49e5a4aab37d6e30e0ac55e33f
Sergey Antipov <trasivasi@gmail.com>
2023-03-07 13:34:36 +0300

    added Home Task 27 of Seminar4

---
+++ Seminar_4/HomeTask_27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber()
+{
+    Console.Write("Введите целое число > ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+void PrintSumDigits(int number)
+{
+    int sum = 0;
+    int temp = number;
+    while (temp >= 1)
+    {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    Console.WriteLine($"Сумма всех цифр числа {number} равна {sum}");
+}
+
+
+int num = InputNumber();
+PrintSumDigits(num);
