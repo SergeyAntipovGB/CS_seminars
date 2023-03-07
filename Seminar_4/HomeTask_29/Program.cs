b9d6139acc390ffa611a0b3f1d334718f694d96d
Sergey Antipov <trasivasi@gmail.com>
2023-03-07 14:04:44 +0300

    added Home Task 29 of Seminar4

---
+++ Seminar_4/HomeTask_29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8
+// элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i+1} число массива >");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length -1]} ]");
+}
+
+
+int[] matrix = new int[8];
+InputArray(matrix);
+PrintArray(matrix);
