f77bd9e0b53bf7149e2a63d80daa01e41e73e32f
Sergey Antipov <ser_ok@mail.ru>
2023-03-11 18:29:52 +0300

    added Home Task 36 of seminar 5

---
+++ Seminar_5/HomeTask_36/Program.cs
@@ -0,0 +1,34 @@
+﻿// ============================== Задача 36:
+// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArray(int[] myArray, int digits)
+{
+    for (int i = 0; i < digits; i++)
+        myArray[i] = new Random().Next(-99, 100);
+    return myArray;
+}
+
+void PrintArray(int[] myArray)
+{
+    foreach (int item in myArray)
+        Console.Write($"{item} ");
+}
+
+int CointOddElements(int[] myArray)
+{
+    int sum = 0;
+    for (int i = 1; i < myArray.Length; i = i + 2)
+        sum += myArray[i];
+    return sum;
+}
+
+
+Console.Write("Введите количество элементов массива > ");
+int howManyElements = int.Parse(Console.ReadLine());
+int[] myArray = new int[howManyElements];
+myArray = FillArray(myArray, howManyElements);
+PrintArray(myArray);
+Console.WriteLine($"-> {CointOddElements(myArray)}");
